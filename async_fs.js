e727cd9fe92f4b6426efdebb13997c5124f55472
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2018-02-13 18:51:27 -0800

    fixes and plugs
    async_fs fixes, moved plugin control to separate file.

--- async_fs.js
+++ async_fs.js
@@ -5,7 +5,8 @@ exports.readdir = (path, options=null) => new Promise( (res,rej)=>{
 	fs.readdir( path, options, (err,files)=>{
 
 		if ( err )rej(err);
-		res(files);
+		else
+			res(files);
 
 	});
 
@@ -15,10 +16,12 @@ exports.readJSON = path => new Promise( (res,rej)=>{
 
 	fs.readFile( path, (err,data)=>{
 
-		if ( err ) rej(err);
-		let json = JSON.parse( data );
-		res( json );
-
+		if ( err )
+			rej(err);
+		else {
+			let json = JSON.parse( data );
+			res( json );
+		 }
 	});
 
 });
@@ -34,6 +37,7 @@ exports.mkdir = path => new Promise( (res,rej)=> {
 
 	fs.mkdir( path, function( err ) {
 		if ( err ) rej(err);
+		else
 		res();
 	});
 
@@ -44,6 +48,7 @@ exports.writeJSON = (path,data) => new Promise( (res, rej)=>{
 	fs.writeFile( path, JSON.stringify(data), {flag:'w+'}, (err)=>{
 
 		if ( err ) rej(err);
+		else
 		res();
 
 	});
---
+++ plugsupport.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const path = require('path');
+
+let plugins = {};
+
+exports.loadPlugins = function( plugins_dir ) {
+
+	let plugins = {};
+
+	try {
+
+		dirs = fs.readdirSync( plugins_dir );
+	
+		let file, dir, stats;
+		for( let dir of dirs ) {
+
+			dir = path.resolve( plugins_dir, dir );
+			stats = fs.statSync( dir );
+			if ( !stats.isDirectory() ) continue;
+
+			findAndLoad( dir );
+
+		}
+
+	} catch ( err ) {
+		console.log(err);
+	}
+
+	return plugins;
+
+}
+
+// look for a plugin desc file.
+function findAndLoad( dir ) {
+
+	let files = fs.readdirSync( dir );
+	let stats;
+
+	for( let file of files ) {
+
+		try {
+		
+			if ( !(path.extname(file) === '.json')) continue;
+
+			file = path.resolve( dir, file );
+			stats = fs.statSync( file );
+			if ( !stats.isFile() ) continue;
+
+			let desc = loadPlugDesc( file );
+			if ( !desc ) continue;
+
+			// desc file.
+			let plugin = loadPlugin( dir, desc );
+			if ( plugin ) {
+				desc[desc.name] = plugin;
+				return;
+			}
+
+		} catch (err ){
+			console.log(err);
+		}
+
+	}
+
+}
+
+function loadPlugDesc( descPath ) {
+
+	console.log( 'loading: ' + descPath );
+	let data = fs.readFileSync(descPath);
+	let desc = JSON.parse( data );
+
+	if ( !desc.hasOwnProperty( 'plugin')) return null;
+	if ( !desc.hasOwnProperty('name')) desc.name = desc.plugin;
+
+	return desc;
+
+}
+
+// load plugin described by json.
+function loadPlugin( plugDir, desc ){
+
+	try {
+
+		let plugFile = path.resolve( plugDir, desc.plugin );
+		return require( plugFile );
+
+	} catch ( err ){
+		console.log( err );
+	}
+
+	return null;
+
+}
