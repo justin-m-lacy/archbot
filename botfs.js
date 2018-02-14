b0bf79568d0d38a915a0e1c2ed2acf9360e64a60
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2018-02-14 11:24:04 -0800

    fixed DM message errors.

--- botfs.js
+++ botfs.js
@@ -11,12 +11,14 @@ const USERS_DIR = 'users/';
 exports.readData = readData;
 exports.writeData = writeData;
 exports.memberPath = getMemberPath;
+exports.userPath = getUserPath;
+
 exports.pluginDir = getPluginDir;
 exports.getPluginFile = getPluginFile;
 
 
 async function readData( relPath ) {
-	return await fsj.readJSON( path.join( BASE_DIR, relPath ) );
+	return await fsj.readJSON( path.join( BASE_DIR, relPath + '.json' ) );
 }
 
 async function writeData( relPath, data ) {
@@ -24,7 +26,7 @@ async function writeData( relPath, data ) {
 	let absPath = path.join( BASE_DIR, relPath );
 
 	await fsj.mkdir( path.dirname( absPath ) );
-	await fsj.writeJSON( absPath, data );
+	await fsj.writeJSON( absPath + '.json', data );
 
 }
 
@@ -45,6 +47,14 @@ function getGuildDir( guild ) {
 	return path.join( SERVERS_DIR, guild.id );
 }
 
+// path to user not in guild.
+function getUserPath( user ) {
+
+	if ( user == null ) return '';
+	return path.join( USERS_DIR, user.id );
+
+}
+
 // path to member's base guild file.
 function getMemberPath( member ) {
 
@@ -53,6 +63,6 @@ function getMemberPath( member ) {
 
 	let gid = member.guild.id;
 
-	return path.join( SERVERS_DIR, gid, (member.id) + '.json' );
+	return path.join( SERVERS_DIR, gid, (member.id) );
 
 }
