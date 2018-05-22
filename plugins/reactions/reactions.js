cff00df073f9e2ce927062ff60f30afeaf3b7d18
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2018-05-22 01:41:21 -0700

    reactions updates. dispatch fix.

--- plugins/reactions/reactions.js
+++ plugins/reactions/reactions.js
@@ -14,7 +14,7 @@ class GuildReactions {
 				let resp = this.react(m.content);
 				if ( resp != null ) m.channel.send( resp );
 			} catch(e){
-				conosle.log(e);
+				console.log(e);
 			}
 
 		} );
@@ -27,35 +27,113 @@ class GuildReactions {
 
 	}
 
-	async cmdAddReaction( m, substr, react ) {
+	async cmdAddReaction( m, trig, react ) {
 
-		if ( substr == null || react == null ) {
+		if ( trig == null || react == null ) {
 			m.channel.send( 'Usage: !react "string" "response"' );
 			return;
 		}
 
-		await this.addReactData( substr, react );
-		m.channel.send( 'Okie Dokie: ' + substr + " -> " + react );
+		await this.addReactData( trig, react, m.author.id );
+		m.channel.send( 'Okie Dokie: ' + trig + " -> " + react );
 
 	}
 
-	async cmdRmReact( m, str ) {
+	async cmdRmReact( m, trig, which ) {
 
-		try {
+		if ( !trig || !which ) return m.channel.send( 'Usage: !rmreact "string" ["reaction"]' );
 
-			delete this.reactions[str];
-			await this._context.storeKeyData( this._context.getDataKey( 'reactions', 'reactions'), this.reactions );
+		let res = await this.rmReact( trig, which );
+		if ( res ) return m.channel.send('Reaction removed.');
 
-		} catch (e) { console.log(e);}
+		return m.channel.send('Reaction not found.');
 
 	}
 
-	async addReactData( substr, react ) {
+	async cmdReactInfo( m, trig, which ) {
+
+		let info = this.reactInfo( trig, which );
+		if ( !info ) return m.channel.send( 'No reaction found.');
+		if ( typeof info === 'string') return m.channel.send( 'No information about this reaction.');
+		if ( info instanceof Array ) return m.channel.send( `Multiple reactions found for trigger ${trig}.`);
+
+		if ( info.uid ) {
+			let name = await this._context.displayName( info.uid );
+			let resp = name ?  `Reaction created by ${name} (id:${info.uid})` : `Reaction created by user id: ${info.uid}`;
+
+			if ( info.t ) resp += ` @ ${new Date(info.t)}`;
+			return m.channel.send( resp );
+		}
+
+		return m.channel.send( 'No information about this reaction.');
+	}
+
+	async rmReact( trig, which ) {
+
+		trig = trig.toLowerCase();
+		let cur = this.reactions[trig];
+		if ( !cur ) return false;
+
+		if ( !which ) delete this.reactions[trig];
+		else {
+
+			if ( !this.tryRemove(cur,which) ) return false;
+			if ( !cur.r ) delete this.reactions[trig];
+
+		}
+
+		await this._context.storeKeyData( this._context.getDataKey( 'reactions', 'reactions'), this.reactions );
+
+		return true;
+
+	}
+
+	/**
+	 * 
+	 * @param {Object} obj - reaction information object. 
+	 * @param {*} which - string reaction to match.
+	 */
+	tryRemove( obj, which ) {
+
+		let r = obj.r;
+
+		which = which.toLowerCase();
+
+		if ( typeof r === 'string') {
+
+			if ( r.toLowerCase() === which ) {
+				obj.r = null;
+				return true;
+			}
+
+		} else if ( r instanceof Array ) {
+
+			let ind = r.findIndex( (elm)=>{
+				if ( typeof elm === 'string' ) return elm.toLowerCase() === which;
+				return ( typeof elm.r === 'string') && elm.r.toLowerCase() === which;
+			});
+
+			if ( ind >= 0 ) {
+				r.splice( ind, 1 );
+				return true;
+			}
+
+		} else if ( typeof r.r === 'string' && r.r.toLowerCase() === which ) {
+			obj.r = null;
+			return true;
+		}
+
+		return false;
+	}
+
+	async addReactData( trig, react, uid ) {
 
 		try {
 
-			let key = substr.toLowerCase();
-			let cur = this.reactions[ key ];
+			trig = trig.toLowerCase();
+			let cur = this.reactions[ trig ];
+
+			react = { r:react, uid:uid, t:Date.now() };
 
 			if ( cur ) {
 
@@ -69,7 +147,7 @@ class GuildReactions {
 
 			} else {
 
-				this.reactions[ key ] = { "r":react };
+				this.reactions[ trig ] = { "r":react };
 			}
 
 			await this._context.storeKeyData(
@@ -82,13 +160,7 @@ class GuildReactions {
 
 		try {
 			let reactData = await this._context.fetchKeyData( this._context.getDataKey( 'reactions', 'reactions' ) );
-			if ( reactData != null ) {
-
-				var utils = require( '../../jsutils.js');
-				//utils.recurMerge( this.reactions, reactData );
-				this.reactions = reactData;
-
-			}
+			if ( reactData != null ) this.reactions = reactData;
 		} catch ( e ) { console.log(e);}
 
 	}
@@ -107,7 +179,7 @@ class GuildReactions {
 			if ( lower.indexOf(k) >= 0 ) {
 
 				resp = this.tryReact( this.reactions[k] );
-				if ( resp != null ) return resp;
+				if ( resp ) return resp;
 
 			}
 		}
@@ -117,7 +189,7 @@ class GuildReactions {
 			if ( lower.indexOf(k) >= 0 ) {
 
 				resp = this.tryReact( globalReactions[k] );
-				if ( resp != null ) return resp;
+				if ( resp ) return resp;
 
 			}
 
@@ -132,15 +204,40 @@ class GuildReactions {
 		if ( !(obj instanceof Object) ) return;
 
 		let last = obj.last;
-		if ( last != null && (this.lastMsg - last) < this.minWait ) return null;
+		if ( last && (this.lastMsg - last) < this.minWait ) return null;
 		obj.last = this.lastMsg;
 
 		let r = obj.r;
 		if ( r instanceof Array ) {
-			return r[ Math.floor( r.length*Math.random() )];
-		} else {
-			return r;
+			r = r[ Math.floor( r.length*Math.random() )];
 		}
+		if ( typeof r === 'string') return r;
+		return r.r;
+
+	}
+
+	reactInfo( trig, which ) {
+
+		trig = trig.toLowerCase();
+		let obj = this.reactions[trig] || globalReactions[trig];
+		if ( !obj ) return;
+		if ( !which ) return obj.r;
+
+		which = which.toLowerCase();
+		let r = obj.r;
+		if ( typeof r === 'string') {
+
+			if ( which === r.toLowerCase() ) return r;
+
+		} else if ( r instanceof Array ) {
+
+			return r.find( (elm)=>{
+				if ( typeof elm === 'string') return elm.toLowerCase() === which;
+				else return ( typeof elm.r === 'string' && elm.r.toLowerCase() === which );
+			});
+
+		} else if ( typeof r.r === 'string' && r.r.toLowerCase() === which ) return r;
+
 
 	}
 
@@ -156,8 +253,11 @@ exports.init = function( bot ) {
 	bot.addContextClass( GuildReactions );
 	bot.addContextCmd( 'react', '!react <\"search trigger\"> <\"response string\">',
 		GuildReactions.prototype.cmdAddReaction, GuildReactions, { minArgs:2, maxArgs:2, group:'right'} );
-	bot.addContextCmd( 'rmreact', '!rmreact <\"react trigger\">', GuildReactions.prototype.cmdRmReact, GuildReactions,
-		{ maxArgs:1 } );
+	bot.addContextCmd( 'reactinfo', '!reactinfo <\"trigger"\"> [response]',
+		GuildReactions.prototype.cmdReactInfo, GuildReactions, { minArgs:1, maxArgs:2, group:'right'});
+
+	bot.addContextCmd( 'rmreact', '!rmreact <\"react trigger\"> [response]', GuildReactions.prototype.cmdRmReact, GuildReactions,
+		{ minArgs:1, maxArgs:2, group:'right' } );
 
 
 }
