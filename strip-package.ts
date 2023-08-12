569f1ddd954b11988e810ee4ae7693e85d0fb1b5
Justin Lacy <justin@launchbadge.com>
2023-08-12 02:51:39 -0700

    script to create package for deploy

---
+++ strip-package.ts
@@ -0,0 +1,30 @@
+import { writeFile } from "fs/promises";
+import { resolve } from "path";
+import mainPackage from "package.json";
+
+/**
+ * Strip package of values not needed for compiled app,
+ * and changes scripts to run archbot.s from the package's director. 
+ * @param packagePath 
+ */
+export const stripPackage = async (outputDir: string = './dist') => {
+
+    const newPackage: any = Object.assign({}, mainPackage);
+
+    /// compiled executable in same directory as compiled package.
+    newPackage.main = "./archbot.js";
+
+    /// compiled version has no devDependencies.
+    newPackage.devDependencies = undefined;
+    newPackage.bugs = undefined;
+    newPackage.repository = undefined;
+    newPackage.license = undefined;
+    newPackage.keywords = undefined;
+
+    newPackage.scripts = {
+        start: "node ."
+    };
+
+    await writeFile(resolve(outputDir), JSON.stringify(newPackage));
+
+}
